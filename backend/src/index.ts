16dde7192ee6887fad674963c6491ae853887507
Akhand Pratap Singh <ankitsingha4p7s@gmail.com>
2025-06-15 22:33:57 +0530

    importing library

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { Hono } from 'hono';
+import { sign } from 'hono/jwt'
 
 const app = new Hono<{
 	
