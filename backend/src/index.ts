640417686298d0cb3c4334700850fd5b172e6935
Akhand Pratap Singh <ankitsingha4p7s@gmail.com>
2025-06-16 19:55:24 +0530

    initiated middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt'
+import { sign, verify } from 'hono/jwt'
 
 const app = new Hono<{
 	Bindings: {
@@ -9,6 +9,12 @@ const app = new Hono<{
 		JWT_SECRET: string,
 	}
 }>();
+
+app.use('/api/v1/blog/*', async (c, next) => {
+	const header = c.req.header('Authorization');
+	const response =await verify(header, c.env.JWT_SECRET);
+  await next()
+})
 
 
 app.post('/api/v1/signup', async (c) => {
