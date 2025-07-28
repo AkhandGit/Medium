147102148527d8064e1625ea094f08e1f65e6499
Akhand Pratap Singh <ankitsingha4p7s@gmail.com>
2025-07-28 20:45:40 +0530

    fix: update package name from @100xdevs/medium-common to @akhandnpm/medium-common

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { sign, verify } from 'hono/jwt'
-import { signupInput } from '@100xdevs/medium-common';
+import { signupInput } from '@akhandnpm/medium-common';
 
 export const userRouter = new Hono<{
 	Bindings: {
