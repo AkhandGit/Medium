07a13236dc04fff1a8dddbd3b272ebf878a8b910
Akhand Pratap Singh <ankitsingha4p7s@gmail.com>
2025-08-01 19:52:53 +0530

    refactor: enhance layout and styling of Signup and Quote components

--- Frontend/src/Pages/Signup.tsx
+++ Frontend/src/Pages/Signup.tsx
@@ -1,8 +1,17 @@
+import { Quote } from "../components/Quote"
+
 export const Signup = () => {
   return (
     <div>
-      <h1>Signup Page</h1>
-      <p>Please fill out the form to create a new account.</p>
+      <div className="grid grid-cols-2">
+        <div>
+          <h1 className="text-3xl font-bold">Signup Page</h1>
+          <p className="mt-2">Please fill out the form to create an account.</p>
+        </div>
+        <Quote/>
+      </div>
+      
+      <Quote/>
     </div>
   )
 }
--- Frontend/src/components/Quote.tsx
+++ Frontend/src/components/Quote.tsx
@@ -1,8 +1,10 @@
 export const Quote = () => {
   return (
-    <div className="bg-slate-200 h-screen">
-      <h1>Quote Page</h1>
-      <p>This is the quote page where you can find inspiring quotes.</p>
+    <div className="bg-slate-200 h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div className="max-w-md text-center"><p>This is the quote page where you can find inspiring quotes.</p>
+        </div>
+      </div>
     </div>
   )
 }
