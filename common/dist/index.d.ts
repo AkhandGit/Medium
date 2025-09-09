fcbaa33fc10a351fae6931127c322ea4fcfa341a
Akhand Pratap Singh <ankitsingha4p7s@gmail.com>
2025-09-09 11:37:06 +0530

    c

--- common/dist/index.d.ts
+++ common/dist/index.d.ts
@@ -1,24 +1,34 @@
 import z from 'zod';
+
+// ------------------- SIGNUP -------------------
 export declare const signupInput: z.ZodObject<{
     username: z.ZodString;
     password: z.ZodString;
     name: z.ZodOptional<z.ZodString>;
 }, z.core.$strip>;
+
+// ------------------- SIGNIN -------------------
 export declare const signinInput: z.ZodObject<{
-    email: z.ZodString;
+    username: z.ZodString;
     password: z.ZodString;
     rememberMe: z.ZodOptional<z.ZodBoolean>;
     captcha: z.ZodOptional<z.ZodString>;
 }, z.core.$strip>;
+
+// ------------------- BLOG -------------------
 export declare const createBlogInput: z.ZodObject<{
     title: z.ZodString;
     content: z.ZodString;
 }, z.core.$strip>;
+
+
 export declare const updateBlogInput: z.ZodObject<{
     title: z.ZodString;
     content: z.ZodString;
     id: z.ZodNumber;
 }, z.core.$strip>;
+
+// ------------------- TYPES -------------------
 export type SignupInput = z.infer<typeof signupInput>;
 export type SigninInput = z.infer<typeof signinInput>;
 export type CreateBlogInput = z.infer<typeof createBlogInput>;
